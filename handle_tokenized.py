10ba59f0f91621678b9b1943064fec1130dcf8c3
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-07 14:54:55 -0800

    Update handle_tokenized.py

--- handle_tokenized.py
+++ handle_tokenized.py
@@ -90,11 +90,12 @@ if __name__ == "__main__":
     # )
 
     # 2. Download the prefix uploads/local_shards from S3 into a local folder
+    # download_folder_from_s3(
+    #     bucket_name="dataframes--use1-az6--x-s3",
+    #     s3_prefix="uploads/local_shards/",
+    #     local_folder_path="./local_shards"
+    # )
     download_folder_from_s3(
-        bucket_name="dataframes--use1-az6--x-s3",
-        s3_prefix="uploads/local_shards/",
-        local_folder_path="./local_shards"
-    )    download_folder_from_s3(
         bucket_name="dataframes--use1-az6--x-s3",
         s3_prefix="uploads/finetune_shards/",
         local_folder_path="./local_shards"
