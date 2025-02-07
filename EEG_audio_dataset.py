2308a22efa3c74cfdea85393d8af922bfe335e01
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-06 18:54:16 -0800

    Update EEG_audio_dataset.py

--- EEG_audio_dataset.py
+++ EEG_audio_dataset.py
@@ -276,8 +276,8 @@ if __name__ == "__main__":
             # Preprocess the trial (e.g., filtering, downsampling)
             prep_data, new_fs = preprocess_data(trial_T, fs)
             # Reduce to 2 channels by averaging alternate channels
-            twoch_data = average_alternate_channels(prep_data)
-            processed_trials.append(twoch_data)
+            # twoch_data = average_alternate_channels(prep_data)
+            processed_trials.append(prep_data)
 
         if processed_trials:
             # Combine all processed trials (all attention EEG) into one continuous signal.
