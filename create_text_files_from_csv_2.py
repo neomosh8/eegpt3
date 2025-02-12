c6d154ad23bd7a3c371b38720e58f046552c35e2
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-12 13:32:46 -0800

    Update create_text_files_from_csv_2.py

--- create_text_files_from_csv_2.py
+++ create_text_files_from_csv_2.py
@@ -415,9 +415,9 @@ if __name__ == "__main__":
     # folders = list_s3_folders()
     csv_files = []
     i = 1
-    folders = ["ds001971"]
+    folders = ["ds002034"]
 
-    folders_to_delete = ["ds002338", "ds002336","ds001849",]
+    folders_to_delete = ["ds002338", "ds002336","ds001849","ds001971"]
 
     new_folders = [folder for folder in folders if folder not in folders_to_delete]
 
