9e89b45b0c6fcbf68db6c06cf119ccdd2cc93e03
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-12 20:59:18 -0800

    Update handle_tokenized.py

--- handle_tokenized.py
+++ handle_tokenized.py
@@ -83,18 +83,18 @@ def download_folder_from_s3(bucket_name: str, s3_prefix: str, local_folder_path:
 if __name__ == "__main__":
     # Example usage:
     # 1. Upload the folder ./local_shards to S3
-    # upload_folder_to_s3(
-    #     local_folder_path="./output_finetue",
-    #     bucket_name="dataframes--use1-az6--x-s3",
-    #     s3_prefix="uploads/finetune_shards"
-    # )
+    upload_folder_to_s3(
+        local_folder_path="./local_shards",
+        bucket_name="dataframes--use1-az6--x-s3",
+        s3_prefix="uploads/local_shards"
+    )
 
     # 2. Download the prefix uploads/local_shards from S3 into a local folder
-    download_folder_from_s3(
-        bucket_name="dataframes--use1-az6--x-s3",
-        s3_prefix="uploads/local_shards/",
-        local_folder_path="./local_shards"
-    )
+    # download_folder_from_s3(
+    #     bucket_name="dataframes--use1-az6--x-s3",
+    #     s3_prefix="uploads/local_shards/",
+    #     local_folder_path="./local_shards"
+    # )
     # download_folder_from_s3(
     #     bucket_name="dataframes--use1-az6--x-s3",
     #     s3_prefix="uploads/finetune_shards/",
