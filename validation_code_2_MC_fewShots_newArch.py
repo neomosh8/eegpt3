91623aca9884f9add4474e0156fd6720bf7348e1
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-15 21:24:56 -0800

    Update validation_code_2_MC_fewShots_newArch.py

--- validation_code_2_MC_fewShots_newArch.py
+++ validation_code_2_MC_fewShots_newArch.py
@@ -573,4 +573,4 @@ if __name__ == "__main__":
     )
 
     # Run forced-choice evaluation over a desired number of samples.
-    fc_classifier.evaluate(num_samples=100)
+    fc_classifier.evaluate(num_samples=1000)
