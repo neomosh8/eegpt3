27716ae5e98ca0c3ef14a8813aed5918a4d63cfa
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-15 20:38:04 -0800

    Update validation_code_2_MC_fewShots_newArch.py

--- validation_code_2_MC_fewShots_newArch.py
+++ validation_code_2_MC_fewShots_newArch.py
@@ -606,7 +606,7 @@ for epoch in range(epochs):
         shard_paths=[
             "local_shards_val/mydata_train_0.pt",
             "local_shards_val/mydata_train_1.pt",
-            "local_shards_val/mydata_train_2.pt"
+            "local_shards_val/mydata_train_20.pt"
         ],
         device=d,
         segment_size=1032//2  # candidate continuation remains 512 tokens total
