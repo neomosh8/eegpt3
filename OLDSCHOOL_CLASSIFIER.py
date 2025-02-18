2fc0ccd54ced23fb3278cc5fd4f709d82716f7a7
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-18 15:23:33 -0800

    Update OLDSCHOOL_CLASSIFIER.py

--- OLDSCHOOL_CLASSIFIER.py
+++ OLDSCHOOL_CLASSIFIER.py
@@ -302,8 +302,8 @@ def main():
 
     shard_paths = ["./local_shards_val/mydata_train_0.pt", "./local_shards_val/mydata_train_1.pt",
                    "./local_shards_val/mydata_train_2.pt"]
-    train_dataset = ShardDataset(shard_paths, sequence_length=config.block_size, split_ratio=0.8, is_train=True)
-    val_dataset = ShardDataset(shard_paths, sequence_length=config.block_size, split_ratio=0.8, is_train=False)
+    train_dataset = ShardDataset(shard_paths, sequence_length=config.block_size, split_ratio=0.95, is_train=True)
+    val_dataset = ShardDataset(shard_paths, sequence_length=config.block_size, split_ratio=0, is_train=False)
     print("Train label distribution:", Counter(train_dataset.labels.tolist()))
     print("Val label distribution:", Counter(val_dataset.labels.tolist()))
 
