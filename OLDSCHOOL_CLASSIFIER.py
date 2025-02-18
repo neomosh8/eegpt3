cc4e6a1816094ea071439216c8fd348d4f4b876c
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-18 14:09:05 -0800

    Update OLDSCHOOL_CLASSIFIER.py

--- OLDSCHOOL_CLASSIFIER.py
+++ OLDSCHOOL_CLASSIFIER.py
@@ -294,8 +294,8 @@ def evaluate_performance(model, dataloader, device, desc="Evaluation"):
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    checkpoint_path = "./checkpoints/model_00600.pt"
-    checkpoint = torch.load(checkpoint_path, map_location=device)
+    checkpoint_path = "./checkpoints/model_00300.pt"
+    checkpoint = torch.load(checkpoint_path, map_location=device,weights_only=False)
     config = checkpoint['config']
 
     # Step 1: Random GPT
