e559087d3f3209467bd359cfb34f2e5585faa91c
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-20 16:31:57 -0800

    Update model_new_arch.py

--- model_new_arch.py
+++ model_new_arch.py
@@ -443,9 +443,9 @@ class DataLoaderLiteAllInMemory:
 # Training Setup & Loop (No Epochs)
 #########################
 # Training hyperparameters
-B = 8  # micro-batch size (sequences per mini-batch)
+B = 16  # micro-batch size (sequences per mini-batch)
 T = GPTConfig.block_size  # sequence length (tokens per sequence)
-desired_B_eff = 128  # effective batch size (number of sequences per optimizer step)
+desired_B_eff = 64  # effective batch size (number of sequences per optimizer step)
 grad_accum_steps = desired_B_eff // B  # number of micro-steps to accumulate gradients
 if master_process:
     print(f"Using grad_accum_steps: {grad_accum_steps}")
