dbf02709b51aade2f67d5699bd39450271a28e30
Mojtaba <rabiei.mojtaba@gmail.com>
2025-02-24 22:54:55 -0800

    d

--- dataset_preprocess_3.py
+++ dataset_preprocess_3.py
@@ -130,11 +130,11 @@ def download_and_preprocess_s3(bucket_name: str, s3_prefix: str, local_data_dir:
 if __name__ == "__main__":
     BUCKET_NAME = "dataframes--use1-az6--x-s3"
     S3_PREFIX = "output_emotiv/"
-    LOCAL_DIR = "./local_shards_val"
+    LOCAL_DIR = "./local_shards"
     download_and_preprocess_s3(bucket_name=BUCKET_NAME,
                               s3_prefix=S3_PREFIX,
                               local_data_dir=LOCAL_DIR,
                               shard_prefix="mydata",
                               limit_files=None,
-                              val_ratio=0.0,
+                              val_ratio=0.1,
                               resolution=80)
