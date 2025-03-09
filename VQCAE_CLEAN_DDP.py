fe6fba02a79b65b6682ce8271bec4edc8920cf99
Mojtaba <rabiei.mojtaba@gmail.com>
2025-03-08 18:38:08 -0800

    Update VQCAE_CLEAN_DDP.py

--- VQCAE_CLEAN_DDP.py
+++ VQCAE_CLEAN_DDP.py
@@ -439,7 +439,7 @@ def evaluate_codebook_usage(model, data_loader, device, save_path="output/codebo
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", type=str, default="training_data/coeffs/")
-    parser.add_argument("--batch_size", type=int, default=4 * 16)
+    parser.add_argument("--batch_size", type=int, default=2 * 16)
     parser.add_argument("--epochs", type=int, default=100)
     parser.add_argument("--lr", type=float, default=3e-4, help="Base learning rate")
     parser.add_argument("--max_lr", type=float, default=4e-3, help="Peak learning rate")
