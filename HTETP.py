f32251af9aa2476cc28bf9af45f199d038f8cd67
Mojtaba <rabiei.mojtaba@gmail.com>
2025-03-11 18:53:59 -0700

    Update HTETP.py

--- HTETP.py
+++ HTETP.py
@@ -618,7 +618,7 @@ def main():
                         help="Hidden dimension of the model")
     parser.add_argument("--n_heads", type=int, default=4,
                         help="Number of attention heads")
-    parser.add_argument("--n_layers", type=int, default=4,
+    parser.add_argument("--n_layers", type=int, default=2,
                         help="Number of transformer layers")
     parser.add_argument("--max_windows", type=int, default=4,
                         help="Maximum number of windows in sequence")
@@ -628,7 +628,7 @@ def main():
                         help="ID of end-of-sequence token")
 
     # Training parameters
-    parser.add_argument("--batch_size", type=int, default=1,
+    parser.add_argument("--batch_size", type=int, default=2,
                         help="Batch size per GPU")
     parser.add_argument("--seq_length", type=int, default=4608,
                         help="Sequence length for training")
