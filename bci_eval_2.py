703286eb34e85d569640fe39bd10924624dc0a1b
Mojtaba <rabiei.mojtaba@gmail.com>
2025-03-13 07:56:06 -0700

    Update bci_eval_2.py

--- bci_eval_2.py
+++ bci_eval_2.py
@@ -1191,7 +1191,7 @@ def main():
         output_dir=args.output_dir,
         n_shots=args.n_shots,
         n_trials=20,  # You can adjust the number of trials
-        include_random_model=not args.skip_random_model  # Notice the "not" here
+        include_random_model=False  # Notice the "not" here
     )
     print("Evaluation complete!")
 
